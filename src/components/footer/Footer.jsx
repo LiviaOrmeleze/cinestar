79ec6da75de33c82ba0ca6f8c9b2236e082bd506
Aninha <aninha.clara030807@gmail.com>
2025-03-19 17:01:19 -0300

    atentando arrumar o footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,15 @@
-import './Footer.css'
+import './Footer.css';
 
 const Footer = (props) => {
   return (
-    <footer>
-    <p>Feito com ❤️ por  
-        <a href={props.devLink}> {props.devName}</a>
-        </p>
-  </footer>
-  )
+    <footer className="custom-footer">
+      <p>Feito com ❤️ por  
+        <a href={props.devLink} target="_blank" rel="noopener noreferrer"> 
+          {props.devName}
+        </a>
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
