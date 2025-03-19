c08d3da2e8c85e77c55790b0047f0a6e2b6bf048
Livia Ormeleze <liviasenai.ormeleze@gmail.com>
2025-03-19 16:55:00 -0300

    Ajustando os movieCards

--- src/App.jsx
+++ src/App.jsx
@@ -1,100 +1,104 @@
-import { useEffect, useState } from 'react';
-import './App.css'
-import Footer from './components/footer/Footer';
-import MovieCard from './components/movieCard/MovieCard';
-import logo from "./assets/logocine.png"
-import lupa from "./assets/search.svg"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import MovieCard from "./components/movieCard/MovieCard";
+import logo from "./assets/logocine.png";
+import lupa from "./assets/search.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 
 const App = () => {
+  <img src="" alt="" />;
 
-  const [search,setSearch] = useState("");
-  const [movies,setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
 
   //Utilizando chave de API do arquivo .env
   // const apiKey = import.meta.env.VITE_OMDB_API_KEY
-  const apiKey = "e4d577fa"
-  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`
-  
+  const apiKey = "e4d577fa";
+  const apiUrl = `https://omdbapi.com/?apikey=${apiKey}`;
+
   //Alimentando com dados para não ficar nulo com useEffect
-  useEffect(()=> {
-    searchMovies("Barbie")
-}, [])
+  useEffect(() => {
+    searchMovies("Barbie");
+  }, []);
 
-//criando a conexão com a API e trazendo informações
-const searchMovies= async (title)=> {
-const response = await fetch(`${apiUrl}&s=${title}&`)
-const data = await response.json()
+  //criando a conexão com a API e trazendo informações
+  const searchMovies = async (title) => {
+    const response = await fetch(`${apiUrl}&s=${title}&`);
+    const data = await response.json();
 
-  //Alimentando o movies
-  setMovies(data.Search)
+    //Alimentando o movies
+    setMovies(data.Search);
+  };
 
-}
-
-//e = evento| ao clicar ou digitar acontece algo
-const handleKeyPress= (e) => {
-  e.key === "Enter" && searchMovies(search)
-}
+  //e = evento| ao clicar ou digitar acontece algo
+  const handleKeyPress = (e) => {
+    e.key === "Enter" && searchMovies(search);
+  };
 
   return (
-    <div id='app'>
+    <div id="app">
+      <img
+        className="logo object-fit-cover"
+        width={"400px"}
+        height={"150px"}
+        src={logo}
+        alt=""
+      />
 
-      <img className='logo' src={logo} alt="" />
+      {/* <div className="search">
+        <input
+          onKeyDown={handleKeyPress}
+          onChange={(e) => setSearch(e.target.value)}
+          type="text"
+          placeholder="Pesquise por filmes"
+        />
+        <img onClick={() => searchMovies(search)} src={lupa} alt="" />
+      </div> */}
 
-      <div className='search'>
-      <input 
-      onKeyDown={handleKeyPress} 
-      onChange={(e)=>setSearch(e.target.value)} type="text" placeholder='Pesquise por filmes' />
-      <img onClick={() => searchMovies(search)} src={lupa} alt="" />
-      </div>
+      <h2 className="empty">Top mais assistidos</h2>
 
-    <h2 className='empty'>Top mais assistidos</h2>
+      {movies?.length > 0 ? (
+        <div className="overflow-x-auto overflow-y-hidden d-flex flex-nowrap g-4 p-4 w-100 removeScroll">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} apiUrl={apiUrl} {...movie} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="empty">😒 Filme não encontrado 😒</h2>
+      )}
 
-    {movies?.length > 0 ? (
-      <div className="container overflow-x: scroll">
-    {movies.map((movie, index) => (
-      <MovieCard key={index} apiUrl={apiUrl} {...movie} />
-    ))}
+      <h2 className="empty">Romances</h2>
 
-   </div> 
-    ) : ( 
-      <h2 className='empty'>😒 Filme não encontrado 😒</h2>
-    )}
+      {movies?.length > 0 ? (
+        <div className="overflow-x-auto overflow-y-hidden d-flex flex-nowrap g-4 p-4 w-100 removeScroll">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} apiUrl={apiUrl} {...movie} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="empty">😒 Filme não encontrado 😒</h2>
+      )}
 
-    <h2 className='empty'>Romances</h2>
+      <h2 className="empty">Ação</h2>
 
-    {movies?.length > 0 ? (
-      <div className="container">
-    {movies.map((movie, index) => (
-      <MovieCard key={index} apiUrl={apiUrl} {...movie} />
-    ))}
-
-   </div> 
-    ) : ( 
-      <h2 className='empty'>😒 Filme não encontrado 😒</h2>
-    )}
-
-<h2 className='empty'>Ação</h2>
-
-{movies?.length > 0 ? (
-  <div className="container">
-{movies.map((movie, index) => (
-  <MovieCard key={index} apiUrl={apiUrl} {...movie} />
-))}
-
-</div> 
-) : ( 
-  <h2 className='empty'>😒 Filme não encontrado 😒</h2>
-)}
+      {movies?.length > 0 ? (
+        <div className="overflow-x-auto overflow-y-hidden d-flex flex-nowrap g-4 p-4 w-100 removeScroll">
+          {movies.map((movie, index) => (
+            <MovieCard key={index} apiUrl={apiUrl} {...movie} />
+          ))}
+        </div>
+      ) : (
+        <h2 className="empty">😒 Filme não encontrado 😒</h2>
+      )}
 
       <Footer
-      devName={'LiviaOrmeleze'}
-      devLink={"https://github.com/LiviaOrmeleze"}
+        devName={"LiviaOrmeleze"}
+        devLink={"https://github.com/LiviaOrmeleze"}
       />
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
--- src/components/movieCard/MovieCard.jsx
+++ src/components/movieCard/MovieCard.jsx
@@ -1,40 +1,41 @@
-import styles from "./MovieCard.module.css"
-import MovieDescription from "../movieDescription/MovieDescription"
-import { useState } from "react"
+import styles from "./MovieCard.module.css";
+import MovieDescription from "../movieDescription/MovieDescription";
+import { useState } from "react";
 
 const MovieCard = (props) => {
-  const [isModalOpen,setIsModalOpen]= useState(false)
-   
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const toggleModal = () => {
-    setIsModalOpen(!isModalOpen)
-  }
+    setIsModalOpen(!isModalOpen);
+  };
   return (
-    <> 
- <div className={styles.movie} onClick={toggleModal}>
-    <div>
-      <p>{props.Year}</p>
-    </div>
+    <>
+      <div
+        className={`flex-shrink-0 m-3 overflow-hidden position-relative border-0 ${styles.movie}`}
+        onClick={toggleModal}
+      >
+        <div>
+          <p>{props.Year}</p>
+        </div>
 
-<div>
-    <img src={props.Poster} alt="" />
-</div>
+        <div className="d-flex justify-content-center">
+          <img className="object-fit-cover w-75" src={props.Poster} alt="" />
+        </div>
 
-    <div>
-    <span>{props.Type}</span>
-    <h3 >{props.Title}</h3>
-    </div>
-  </div>
-{isModalOpen && (
-
-  <MovieDescription
-  click={toggleModal}
-  apiUrl={props.apiUrl} 
-  movieID={props.imdbID}
-  />
-)
-}
+        <div className="hoverT border-0">
+          <span>{props.Type}</span>
+          <h3>{props.Title}</h3>
+        </div>
+      </div>
+      {isModalOpen && (
+        <MovieDescription
+          click={toggleModal}
+          apiUrl={props.apiUrl}
+          movieID={props.imdbID}
+        />
+      )}
     </>
-  )
-}
+  );
+};
 
-export default MovieCard
+export default MovieCard;
