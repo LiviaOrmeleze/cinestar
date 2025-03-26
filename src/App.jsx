1b455ed5a79e03cb34649babefe2bacdb194dc06
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-03-26 08:43:05 -0300

    Resolvendo possíveis problemas dados após sincronização

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import { useEffect, useState } from 'react';
  import "bootstrap/dist/css/bootstrap.min.css";
  import "bootstrap/dist/js/bootstrap.bundle.js";
  import Search from "./components/search/Search";
-
-
- const App = () => {
  
+ 
+ const App = () => {
 
-   
-  const [search, setSearch] = useState("");
+ 
+  
+   const [search, setSearch] = useState("");
    const [movies, setMovies] = useState([]);
  
    //Utilizando chave de API do arquivo .env
@@ -37,34 +37,39 @@ import { useEffect, useState } from 'react';
    };
  
 
-
-
-  return (
- <div id='app'>
+ 
+   return (
+ 
+ 
+     <div id='app'>
  
        <div className='top-container'>
        <Search className="p-2"search={search} setSearch={setSearch} searchMovies={searchMovies} lupa={lupa} />
        <Header 
        headerPerfil={perfil} />
        </div>
-
-       {movies?.length > 0 ? (
+ 
+ 
+ 
+ 
+ 
+     {movies?.length > 0 ? (
        <div className="container">
      {movies.map((movie, index) => (
        <MovieCard key={index} apiUrl={apiUrl} {...movie} />
-     
+ 
      ))}
     </div> 
      ) : ( 
        <h2 className='empty'>Filme não encontrado😞</h2>
      )}
  
- <Footer
+       <Footer
          devName={"LiviaOrmeleze"}
          devLink={"https://github.com/LiviaOrmeleze"}
        />
      </div>
-)
-};
+   );
+ };
  
  export default App;
